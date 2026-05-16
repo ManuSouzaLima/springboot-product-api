232034dad47ced8e994b5a407dbfba43357069c8
Emanuel Souza <emanuelsouza1807@gmail.com>
2026-05-15 23:26:46 -0300

    iniciando projeto spring

---
+++ src/main/java/com/emanuel/desafio_durotech/DesafioDurotechApplication.java
@@ -0,0 +1,13 @@
+package com.emanuel.desafio_durotech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioDurotechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioDurotechApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/emanuel/desafio_durotech/DesafioDurotechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emanuel.desafio_durotech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioDurotechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
